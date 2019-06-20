34c6e5c49c71f65fe441225357d03e139edd1d7d
John Lamb <lambj@intific.com>
2019-06-20 12:16:26 -0500

    built out connection class

--- actor/random_agent.py
+++ actor/random_agent.py
@@ -1,7 +1,43 @@
 import os
+import numpy as np
+import time
+import gym
+import gym_everglades
 
-try:
+
+class RandomAgent:
+    def __init__(self, actions):
+        self.actions = actions
+
+    def get_action(self, obs):
+        return np.random.choice(self.actions)
+
+
+def main():
+    env = gym.make('everglades-v0')
+    obs = env.reset()
+
+    agent = RandomAgent(env.action_space)
+
+    rewards = []
+    done = False
+    st_time = time.time()
+    while not done:
+        action = agent.get_action(obs)
+        obs, reward, done, info = env.step(action)
+        rewards.append(reward)
+    end_time = time.time()
+    print('Game completed')
+    print('FPS: {:.2f}'.format(len(rewards) / (end_time - st_time)))
+    print('Reward:\n\tmean/median: {:.2f}/{:.2f}\n\tmin/max: {:.2f}/{:.2f}'.format(np.mean(rewards), np.median(rewards),
+                                                                                   np.min(rewards), np.max(rewards)))
+
+
+if __name__ == "__main__":
     is_training_env = os.getenv('everglades_agent_is_training', 1)
-    is_training = int(is_training_env) == 1
-except:
-    raise Exception('Unexpected value found in everglades_agent_is_training: {}'.format(is_training_env))
+    try:
+        is_training = int(is_training_env) == 1
+    except:
+        raise Exception('Unexpected value found in everglades_agent_is_training: {}'.format(is_training_env))
+
+    main()
--- env/gym-everglades/gym_everglades/envs/everglades_env.py
+++ env/gym-everglades/gym_everglades/envs/everglades_env.py
@@ -1,5 +1,33 @@
 import gym
+from logging import getLogger
+
+logger = getLogger()
 
 
 class Everglades(gym.Env):
-    pass
+    def __init__(self, player_num, game_config):
+        self.parse_game_config(game_config)
+        self.player_num = player_num
+
+        self.conn = self.start_connection()
+
+    def parse_game_config(self, config):
+        """
+        Loads the dict config file
+        :param config:
+        :return:
+        """
+        self.server_address = config.get('server_address')
+        self.pub_socket = config.get('pub_socket', '5555')
+        self.sub_socket = config.get('sub_socket', '5563')
+
+    def start_connection(self):
+        """
+        Establish connection with game server
+        :return:
+        """
+        pub_addr = f'tcp://*:{self.pub_socket}'
+        sub_addr = f'tcp://{self.server_address}:{self.sub_socket}'
+        logger.debug(f'Connecting to server at:\n\tPub Addr {pub_addr}\n\tSub Addr {sub_addr}')
+
+
